db813275db039b218376c5ea332ba9cf009b4583
Miguel Arriaga Velasco <marriagav3@gmail.com>
2023-03-29 13:14:37 -0600

    cleaner structure

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -2,85 +2,13 @@ import React from "react";
 import CvSection from "./CvSection";
 import useCvSectionList from "../hooks/useCvSectionList";
 import "../styles/styles.css";
-import { v4 as uuidv4 } from "uuid";
-
-function createTextInput(placeholder: string, value: string): textInput {
-  return { placeholder: placeholder, value: "" };
-}
-
-const personalInfoPlaceholders = [
-  "First name",
-  "Last name",
-  "Title",
-  "Address",
-  "Phone number",
-  "Email",
-  "Description",
-];
-
-const experiencePlaceholders = ["Position", "Company", "City", "From", "To"];
-
-const educationPlaceholders = [
-  "University name",
-  "City",
-  "Degree",
-  "Subject",
-  "From",
-  "To",
-];
-
-const personalInfoInputs: textInput[] = personalInfoPlaceholders.map(
-  (placeholder: string) => {
-    return createTextInput(placeholder, "");
-  }
-);
-
-const experienceInputs: textInput[] = experiencePlaceholders.map(
-  (placeholder: string) => {
-    return createTextInput(placeholder, "");
-  }
-);
-
-const educationInputs: textInput[] = educationPlaceholders.map(
-  (placeholder: string) => {
-    return createTextInput(placeholder, "");
-  }
-);
-
-const educationCVSection: cvSection = {
-  sectionHeader: "Education",
-  sectionInputs: educationInputs,
-  hasDeleteButton: true,
-  hasAddButton: true,
-  key: uuidv4(),
-};
-
-const experienceCVSection: cvSection = {
-  sectionHeader: "Experience",
-  sectionInputs: experienceInputs,
-  hasDeleteButton: true,
-  hasAddButton: true,
-  key: uuidv4(),
-};
-
-const infoCVSection: cvSection = {
-  sectionHeader: "Personal Information",
-  sectionInputs: personalInfoInputs,
-  hasDeleteButton: false,
-  hasAddButton: false,
-  key: uuidv4(),
-};
 
 interface InputSectionProps {
   className: string;
 }
 
 function InputSection(props: InputSectionProps) {
-  const { cvSectionList, addCvSection } = useCvSectionList([
-    infoCVSection,
-    experienceCVSection,
-    educationCVSection,
-  ]);
+  const { cvSectionList, addCvSection } = useCvSectionList();
 
   let cvSections = cvSectionList?.map((cvSection) => {
     return (
--- src/hooks/useCvSectionList.tsx
+++ src/hooks/useCvSectionList.tsx
@@ -1,8 +1,79 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-function useCvSectionList(svSectionList: cvSection[]) {
-  const [cvSectionList, setCvSections] = useState<cvSection[]>(svSectionList);
+function createTextInput(placeholder: string, value: string): textInput {
+  return { placeholder: placeholder, value: "" };
+}
+
+const personalInfoPlaceholders = [
+  "First name",
+  "Last name",
+  "Title",
+  "Address",
+  "Phone number",
+  "Email",
+  "Description",
+];
+
+const experiencePlaceholders = ["Position", "Company", "City", "From", "To"];
+
+const educationPlaceholders = [
+  "University name",
+  "City",
+  "Degree",
+  "Subject",
+  "From",
+  "To",
+];
+
+const personalInfoInputs: textInput[] = personalInfoPlaceholders.map(
+  (placeholder: string) => {
+    return createTextInput(placeholder, "");
+  }
+);
+
+const experienceInputs: textInput[] = experiencePlaceholders.map(
+  (placeholder: string) => {
+    return createTextInput(placeholder, "");
+  }
+);
+
+const educationInputs: textInput[] = educationPlaceholders.map(
+  (placeholder: string) => {
+    return createTextInput(placeholder, "");
+  }
+);
+
+const educationCVSection: cvSection = {
+  sectionHeader: "Education",
+  sectionInputs: educationInputs,
+  hasDeleteButton: true,
+  hasAddButton: true,
+  key: uuidv4(),
+};
+
+const experienceCVSection: cvSection = {
+  sectionHeader: "Experience",
+  sectionInputs: experienceInputs,
+  hasDeleteButton: true,
+  hasAddButton: true,
+  key: uuidv4(),
+};
+
+const infoCVSection: cvSection = {
+  sectionHeader: "Personal Information",
+  sectionInputs: personalInfoInputs,
+  hasDeleteButton: false,
+  hasAddButton: false,
+  key: uuidv4(),
+};
+
+function useCvSectionList() {
+  const [cvSectionList, setCvSections] = useState<cvSection[]>([
+    infoCVSection,
+    experienceCVSection,
+    educationCVSection,
+  ]);
 
   function addCvSection(cvSection: cvSection) {
     setCvSections(cvSectionList?.concat(cvSection));
